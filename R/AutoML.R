ef122729f5ff7c43fb2d4b36814f7952b39d5044
a-hanf <alexanderhanf@gmx.de>
2020-05-25 13:14:42 +0200

    added fixed classification pipeline

--- R/AutoML.R
+++ R/AutoML.R
@@ -18,11 +18,27 @@ AutoML = R6Class(
       measures = NULL, param_set = NULL, terminator = NULL) {
       private$..perform_input_checks(task, learner, resampling, measures, param_set, terminator)
       self$task = task
-      self$learner = learner
-      self$resampling = resampling
-      self$measures = measures
-      self$param_set = param_set
-      self$tuning_terminator = terminator
+      self$learner = if (!is.null(learner)) learner else private$..get_default_learner()
+      self$resampling = if (!is.null(resampling)) resampling else rsmp("holdout", ratio = 0.8)
+      self$measures = if (!is.null(measures)) measures else mlr_measures$get("classif.acc")
+      self$param_set = if (!is.null(param_set)) param_set else ParamSet$new(list(
+        ParamDbl$new("classif.rpart.cp", lower = 0.001, upper = 0.1),
+        ParamInt$new("classif.rpart.minsplit", lower = 1, upper = 10)
+      ))
+      self$tuning_terminator = if (!is.null(terminator)) terminator else term("clock_time", secs = 10)
+      self$tuner <- tnr("random_search")
+    },
+    train = function() {
+      tuning <- TuningInstance$new(
+        task = self$task,
+        learner = self$learner,
+        resampling = self$resampling,
+        measures = self$measures,
+        param_set = self$param_set,
+        terminator = self$tuning_terminator
+      )
+      self$tuner$tune(tuning)
+      self$learner$param_set$values = tuning$result$params
     }
   ),
   private = list(
@@ -34,10 +50,15 @@ AutoML = R6Class(
       if (!is.null(param_set)) assert_param_set(param_set)
       # FIXME: find / write assertion for terminator class
       # if (!is.null(terminator)) assert_terminator(terminator)
+    },
+    ..get_default_learner = function() {
+      pipeline = po("imputemedian") %>>%
+        po("learner", learner = mlr_learners$get("classif.rpart"))
+      return(GraphLearner$new(pipeline))
     }
   )
 )
 
 iris <- mlr3::tsk("iris")
 auto_iris <- AutoML$new(iris)
-print(auto_iris)
+best_model <- auto_iris$train()
