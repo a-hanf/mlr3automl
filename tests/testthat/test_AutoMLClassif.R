708da39f1a449fb3dfb306186ed2b6ff5daf4657
a-hanf <alexanderhanf@gmx.de>
2020-06-03 13:45:25 +0200

    more tests

--- tests/testthat/test_AutoMLClassif.R
+++ tests/testthat/test_AutoMLClassif.R
@@ -1,43 +1,24 @@
 test_that("basic example works", {
-  expect_r6(
-    {
-      auto_iris <- AutoML(mlr3::tsk("iris"))
-    },
-    "AutoMLClassif"
-  )
-  train_set <- sample(auto_iris$task$nrow, 0.8 * auto_iris$task$nrow)
-  test_set <- setdiff(seq_len(auto_iris$task$nrow), train_set)
-  auto_iris$train(train_set)
-  # check train performance
-  expect_gt(auto_iris$learner$model$tuning_instance$result$perf, 0.8)
-  # check test performance
-  prediction <- auto_iris$predict(row_ids = test_set)
-  expect_gt(prediction$score(auto_iris$measures), 0.8)
 
-  # # TODO: fix this test, currently AutoML breaks whenever data is missing
-  # ind = rnorm(150) > 2
-  # iris[ind, "Sepal.Length"] = NA
-  # missing_iris = TaskClassif$new("missing_iris", backend = iris, target = "Species")
-  # expect_r6({auto_iris = AutoML(missing_iris)}, "AutoMLClassif")
-  # train_set = sample(auto_iris$task$nrow, 0.8 * auto_iris$task$nrow)
-  # test_set = setdiff(seq_len(auto_iris$task$nrow), train_set)
-  # auto_iris$train(train_set)
-  # # check train performance
-  # expect_gt(auto_iris$learner$model$tuning_instance$result$perf, 0.8)
-  # # check test performance
-  # prediction = auto_iris$predict(row_ids = test_set)
-  # expect_gt(prediction$score(auto_iris$measures), 0.8)
-
-  test_openml_task <- function(task_id, min_performance) {
-    task <- tsk("oml", data_id = task_id)
-    model <- AutoML(task)
-    train_set <- sample(model$task$nrow, 0.8 * model$task$nrow)
-    test_set <- setdiff(seq_len(model$task$nrow), train_set)
+  test_standard_task = function(task_type, task_id, min_performance) {
+    task = tsk(task_type, task_id)
+    model = AutoML(task)
+    train_set = sample(model$task$nrow, 0.8 * model$task$nrow)
+    test_set = setdiff(seq_len(model$task$nrow), train_set)
     model$train(train_set)
     expect_gt(model$learner$model$tuning_instance$result$perf, min_performance)
-    prediction <- model$predict(row_ids = test_set)
+    prediction = model$predict(row_ids = test_set)
     expect_gt(prediction$score(model$measures), min_performance)
   }
+
+  test_standard_task("iris", "iris", 0.9)
+  test_standard_task("german_credit", "german_credit", 0.7)
+  # missing data -> error in PipeOp$train: Assertion on 'input 1 ("input") of PipeOp classif.rpart.removeconstants
+  # test_standard_task("pima", "pima", 0.8)
+  test_standard_task("sonar", "sonar", 0.7)
+  test_standard_task("spam", "spam", 0.8)
+  test_standard_task("wine", "wine", 0.8)
+  test_standard_task("zoo", "zoo", 0.8)
 
   # taken from https://www.openml.org/search?q=tags.tag%3Astudy_218&type=data&table=1&size=39
   # as referenced in https://openml.github.io/automlbenchmark/benchmark_datasets.html
--- tests/testthat/test_AutoMLRegr.R
+++ tests/testthat/test_AutoMLRegr.R
@@ -1,24 +1,18 @@
 test_that("basic example works", {
-  expect_r6({auto_mtcars = AutoML(mlr3::tsk("mtcars"))}, "AutoMLRegr")
-  train_set = sample(auto_mtcars$task$nrow, 0.8 * auto_mtcars$task$nrow)
-  test_set = setdiff(seq_len(auto_mtcars$task$nrow), train_set)
-  auto_mtcars$train(train_set)
-  # check train performance
-  expect_lt(auto_mtcars$learner$model$tuning_instance$result$perf, 5)
-  # check test performance
-  prediction = auto_mtcars$predict(row_ids = test_set)
-  expect_lt(prediction$score(auto_mtcars$measures), 5)
 
-  test_openml_task = function(task_id, min_performance) {
-    task = tsk("oml", data_id = task_id)
+  test_standard_task = function(task_type, task_id, min_performance) {
+    task = tsk(task_type, task_id)
     model = AutoML(task)
     train_set = sample(model$task$nrow, 0.8 * model$task$nrow)
     test_set = setdiff(seq_len(model$task$nrow), train_set)
     model$train(train_set)
-    expect_gt(model$learner$model$tuning_instance$result$perf, min_performance)
+    expect_lt(model$learner$model$tuning_instance$result$perf, min_performance)
     prediction = model$predict(row_ids = test_set)
-    expect_gt(prediction$score(model$measures), min_performance)
+    expect_lt(prediction$score(model$measures), min_performance)
   }
+
+  test_standard_task("mtcars", "mtcars", 5)
+  test_standard_task("boston_housing", "boston_housing", 5)
 
   # none of these work: Error in open.connection(con, "rb") : HTTP error 412.
   # test_openml_task(2295, 50)
