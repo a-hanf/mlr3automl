b2b11f9de36462d01c024b0867c597675ceda0b5
a-hanf <alexanderhanf@gmx.de>
2020-06-07 11:58:10 +0200

    use resampling endpoint for tests

--- tests/testthat/test_AutoMLClassif.R
+++ tests/testthat/test_AutoMLClassif.R
@@ -1,37 +1,33 @@
 test_that("basic example works", {
 
-  test_standard_task = function(task_type, task_id, min_performance) {
+  test_classification_task = function(task_type, task_id, min_performance) {
     task = tsk(task_type, task_id)
     model = AutoML(task)
-    train_set = sample(model$task$nrow, 0.8 * model$task$nrow)
-    test_set = setdiff(seq_len(model$task$nrow), train_set)
-    model$train(train_set)
+    result = model$resample()
     expect_gt(model$learner$model$tuning_instance$result$perf, min_performance)
-    prediction = model$predict(row_ids = test_set)
-    expect_gt(prediction$score(model$measures), min_performance)
+    expect_gt(result$aggregate(model$measures), min_performance)
+    invisible(NULL)
   }
 
-  test_standard_task("iris", "iris", 0.9)
-  test_standard_task("german_credit", "german_credit", 0.7)
+  test_classification_task("iris", "iris", 0.9)
+  test_classification_task("german_credit", "german_credit", 0.7)
   # missing data -> error in PipeOp$train: Assertion on 'input 1 ("input") of PipeOp classif.rpart.removeconstants
-  # test_standard_task("pima", "pima", 0.8)
-  test_standard_task("sonar", "sonar", 0.7)
-  test_standard_task("spam", "spam", 0.8)
-  test_standard_task("wine", "wine", 0.8)
-  test_standard_task("zoo", "zoo", 0.8)
+  # test_classification_task("pima", "pima", 0.8)
+  test_classification_task("sonar", "sonar", 0.7)
+  test_classification_task("spam", "spam", 0.8)
+  test_classification_task("wine", "wine", 0.8)
+  test_classification_task("zoo", "zoo", 0.8)
 
   # taken from https://www.openml.org/search?q=tags.tag%3Astudy_218&type=data&table=1&size=39
   # as referenced in https://openml.github.io/automlbenchmark/benchmark_datasets.html
-  test_openml_task(12, 0.75)
-  test_openml_task(31L, 0.55)
-  test_openml_task(53, 0.6)
-
-  # these tasks don't run with a 412 response code - offline on OpenML?
-  # test_openml_task(10101L, 0.7)
-  # test_openml_task(9952L, 0.7)
-  # test_openml_task(145854, 0.85)
-  # test_openml_task(14965, 0.85)t
-  # test_openml_task(34539, 0.8)
-  # test_openml_task(9981, 0.55)
-  # test_openml_task(9952L, 0.7)
+  test_classification_task("oml", 12, 0.75)
+  test_classification_task("oml", 31L, 0.55)
+  test_classification_task("oml", 53, 0.6)
+  test_classification_task("oml", 10101L, 0.7)
+  test_classification_task("oml", 9952L, 0.7)
+  test_classification_task("oml", 145854, 0.85)
+  test_classification_task("oml", 14965, 0.85)
+  test_classification_task("oml", 34539, 0.8)
+  test_classification_task("oml", 9981, 0.55)
+  test_classification_task("oml", 9952L, 0.7)
 })
--- tests/testthat/test_AutoMLRegr.R
+++ tests/testthat/test_AutoMLRegr.R
@@ -1,23 +1,18 @@
 test_that("basic example works", {
 
-  test_standard_task = function(task_type, task_id, min_performance) {
+  test_regression_task = function(task_type, task_id, min_performance) {
     task = tsk(task_type, task_id)
     model = AutoML(task)
-    train_set = sample(model$task$nrow, 0.8 * model$task$nrow)
-    test_set = setdiff(seq_len(model$task$nrow), train_set)
-    model$train(train_set)
+    result = model$resample()
     expect_lt(model$learner$model$tuning_instance$result$perf, min_performance)
-    prediction = model$predict(row_ids = test_set)
-    expect_lt(prediction$score(model$measures), min_performance)
+    expect_lt(result$aggregate(model$measures), min_performance)
+    invisible(NULL)
   }
 
-  test_standard_task("mtcars", "mtcars", 5)
-  test_standard_task("boston_housing", "boston_housing", 5)
-
-  # none of these work: Error in open.connection(con, "rb") : HTTP error 412.
-  # test_openml_task(2295, 50)
-  # test_openml_task(52948, 5)
-  # test_openml_task(4823, 0.75)
-  # test_openml_task(2285 , 0.75)
-  # test_openml_task(4729, 200)
+  test_regression_task("mtcars", "mtcars", 5)
+  test_regression_task("boston_housing", "boston_housing", 5)
+  test_regression_task("oml", 2295, 50)
+  test_regression_task("oml", 52948, 5)
+  test_regression_task("oml", 4823, 0.75)
+  test_regression_task("oml", 4729, 200)
 })
